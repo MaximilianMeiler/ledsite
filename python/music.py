634f7d77344bc45dfa8636a7747bc2a1441bee8b
MaximilianMeiler <maximilianmeiler@ufl.edu>
2023-01-01 08:27:05 -0500

    funcy

--- python/music.py
+++ python/music.py
@@ -1,62 +1,41 @@
-# %%
+
 import librosa.display
 import librosa
 import matplotlib.pyplot as plt
 import numpy as np
 import time
 
-# %%
 multiplier = 8
-y, sr = librosa.load('/Users/maxim/Videos/IM.mp3', sr=2045*multiplier)
-tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
-beat_times = librosa.frames_to_time(beat_frames, sr=sr)
 
-# %%
-spec = librosa.feature.melspectrogram(y=y, sr=sr)
-librosa.display.specshow(spec,y_axis='mel', x_axis='s', sr=sr)
-db_spec = librosa.power_to_db(spec, ref=np.max,)
+def getSongArray(filename):
+  #load a track
+  y, sr = librosa.load(filename, sr=2045*multiplier)
 
-for i,x in enumerate(db_spec):
-  db_spec[i] = x[::1]
+  #create a np array
+  spec = librosa.feature.melspectrogram(y=y, sr=sr)
+  librosa.display.specshow(spec,y_axis='mel', x_axis='s', sr=sr)
+  db_spec = librosa.power_to_db(spec, ref=np.max,)
 
-#librosa.display.specshow(db_spec,y_axis='mel', x_axis='s', sr=sr)
-#plt.colorbar();
+  print(sr)
 
-# %%
-print(sr)
+  #get the sample count
+  count = 0
+  for x in db_spec[0]:
+    count += 1
+  print(count)
 
-count = 0
-for x in db_spec[0]:
-  count += 1
-print(count)
+  #create an array of normalized pitches per time
+  array = [[None]*128 for i in range(count)]
+  for i in range(count):
+    for j,x in enumerate(db_spec):
+      array[i][j] = (x[i] + 80) / 80
 
-for x in beat_times:
-  #print(x)
-  pass
-
-
-# %%
-volArray = [0]*count
-for x in db_spec:
-  for i,y in enumerate(x):
-    volArray[i] = volArray[i] + y + 80
-
-# %%
-#fig, ax = plt.subplots()
-#ax.plot(range(count), volArray)
-#plt.show()
-
-# %%
-array = [[None]*128 for i in range(count)]
+  return array
 
 
-for i in range(count):
-  for j,x in enumerate(db_spec):
-    array[i][j] = (x[i] + 80) / 80
 
-#print(array)
+array = getSongArray('/Users/maxim/Videos/IM.mp3')
 
-# %%
 for x in array:
   for y in x:
     if y < .1:
